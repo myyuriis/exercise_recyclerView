7e0d1c8d9f635c6c124120b7fdb2005cc7275ee4
Muhammad Yusril Yuriis <61834564+myyuriis@users.noreply.github.com>
2020-08-15 13:24:40 +0700

    add button

--- app/src/main/java/com/example/exercise_recyclerview/LanguageRecycleAdapter.kt
+++ app/src/main/java/com/example/exercise_recyclerview/LanguageRecycleAdapter.kt
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 
 class LanguageRecycleAdapter(private val languageList: MutableList<String>): RecyclerView.Adapter<LanguageViewHolder>() {
 
@@ -19,12 +20,18 @@ class LanguageRecycleAdapter(private val languageList: MutableList<String>): Rec
     }
 
     override fun onBindViewHolder(holder: LanguageViewHolder, position: Int) {
+        println("$position ${languageList[position]}")
         holder.orderNumber.text = position.toString()
         holder.languageName.text = languageList[position]
+        holder.itemView.setOnClickListener(holder)
     }
 }
 
-class LanguageViewHolder(v: View): RecyclerView.ViewHolder(v) {
+class LanguageViewHolder(v: View): RecyclerView.ViewHolder(v), View.OnClickListener {
     val orderNumber: TextView = v.findViewById<TextView>(R.id.order_number)
     val languageName: TextView = v.findViewById<TextView>(R.id.language_name)
+
+    override fun onClick(p0: View?) {
+        Toast.makeText(p0?.context, "${languageName.text}", Toast.LENGTH_SHORT).show()
+    }
 }
--- app/src/main/java/com/example/exercise_recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/exercise_recyclerview/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.exercise_recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var languageList = mutableListOf<String>("Java", "Python", "C", "C++", "C#", "Objective C", "Golang", "Cobol", "Assembly", "Javascript", "Dart", "Swift")
+    lateinit var languageRecycleAdapter: LanguageRecycleAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        language_recycleview.layoutManager = LinearLayoutManager(this)
+        languageRecycleAdapter = LanguageRecycleAdapter(languageList)
+        language_recycleview.adapter = languageRecycleAdapter
+    }
+
+    fun addLanguage(view: View) {
+        val languageName: String = language_name_input.text.toString()
+        languageList.add(languageName)
+        language_recycleview.adapter?.notifyDataSetChanged()
     }
 }
